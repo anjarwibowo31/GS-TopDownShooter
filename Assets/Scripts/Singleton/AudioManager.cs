d091260f23d9ca31773d7acdaa4768a9c9948398
anjarwibowo31 <anjar31wibowo@gmail.com>
2023-05-23 13:36:15 +0700

    Update AudioManager.cs

--- Assets/Scripts/Singleton/AudioManager.cs
+++ Assets/Scripts/Singleton/AudioManager.cs
@@ -6,7 +6,6 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance {  get; private set; }
 
-    public float MasterVolume { get { return MasterVolume; } set { MasterVolume = 1f; } }
     public float BGMVolume { get { return BGMVolume; } set { BGMVolume = 1f; } }
     public float SFXVolume { get { return SFXVolume; } set { SFXVolume = 1f; } }
 
